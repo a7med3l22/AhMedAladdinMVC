1227e448a036c5ca6201ea5d0abe0a0d3ebab9fa
justb <justb@DESKTOP-31ALNJD>
2025-04-27 14:26:54 +0300

    DAL Changes

---
+++ AhMedAladdinMVC.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AhMedAladdinMVC.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhMedAladdinMVC.DAL.Data
+{
+	internal class ApplicationDbContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=.;Database=AhMedAladdinMVC;Integrated Security=true;TrustServerCertificate=True");
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+
+		public DbSet<Department> Departments { get; set; }
+
+
+	}
+	
+}
---
+++ AhMedAladdinMVC.DAL/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhMedAladdinMVC.DAL.Models
+{
+	internal class Department
+	{
+		public int Id { get; set; }
+		public string Code { get; set; }
+		public string Name { get; set; }
+		public DateTime DateOfCreation { get; set; }
+
+
+	}
+}
