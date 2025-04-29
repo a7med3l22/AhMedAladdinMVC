c0218741d5476343b568132277c4940dab8ae624
justb <justb@DESKTOP-31ALNJD>
2025-04-29 15:27:34 +0300

    departmentcontrollerview

--- AhMedAladdinMVC.PL/Program.cs
+++ AhMedAladdinMVC.PL/Program.cs
@@ -12,7 +12,7 @@ namespace AhMedAladdinMVC.PL
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<IDepartmentRepository,DeparymentRepository>();
